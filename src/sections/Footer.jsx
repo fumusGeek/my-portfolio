7b51a8af7e1f65fdd73b9a79a1748b8ee7bfd263
fumusGeek <hanifantoni2@gmail.com>
2025-01-08 23:19:48 +0700

    updated: social media

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,23 +4,35 @@ const Footer = () => {
   return (
     <section className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
       <div className="flex gap-3">
-        <div className="social-icon">
+        <a
+          className="social-icon"
+          href="https://github.com/fumusGeek"
+          target="_blank"
+        >
           <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
+        </a>
+        <a
+          className="social-icon"
+          href="https://www.linkedin.com/in/hanif-ifran-a-18684528a?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B0i2AqQWrRKSkJQyvXDsQBg%3D%3D"
+          target="_blank"
+        >
           <img
-            src="/assets/x-social-media-logo-icon.svg"
-            alt="twitter"
+            src="/assets/linkedin-square-icon.svg"
+            alt="linkedin"
             className="w-1/2 h-1/2"
           />
-        </div>
-        <div className="social-icon">
+        </a>
+        <a
+          href="https://www.instagram.com/hnif088"
+          className="social-icon"
+          target="_blank"
+        >
           <img
             src="/assets/instagram.svg"
             alt="instagram"
             className="w-1/2 h-1/2"
           />
-        </div>
+        </a>
       </div>
 
       <p className="text-white-500">© 2024 Hanif. All rights reserved.</p>
